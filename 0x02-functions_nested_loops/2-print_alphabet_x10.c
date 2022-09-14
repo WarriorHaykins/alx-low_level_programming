8d9d4fe5de1b7f24f9f64666a769ce68309ee3ac
WarriorHaykins <harkindutireharkin@gmail.com.com>
2022-09-14 05:50:43 -0700

    print alphabet x10 file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints alphabet in lowercase frim a to z 10 times
+*/
+void print_alphabet_x10(void)
+{
+int i;
+i = 0;
+while (i < 10)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++;
+}
+}
