0672daebca3172870aa23b8b9d78791de2a99326
WarriorHaykins <harkindutireharkin@gmail.com.com>
2022-09-14 04:57:19 -0700

    alphabet file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet - prints a to z in lowercase
+*/
+
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+
+}
+_putchar('\n');
+
+}
