7d9f3d5e4f8791b12812187d50b4432a6398cda8
WarriorHaykins <harkindutireharkin@gmail.com.com>
2022-09-14 14:18:17 -0700

    absolute file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+*_abs - checks the absolute value and prints it
+*Return: returns the absolute value of the integer
+*@r: the integer to be checked
+*/
+
+int _abs(int r)
+{
+if (r >= 0 || r <= 0)
+{
+return (r);
+}
+return (0);
+}
