4a2d2be78deb75d9f0fe51fa974ead5ace51656d
WarriorHaykins <harkindutireharkin@gmail.com.com>
2022-09-15 10:27:39 -0700

    removed mul file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*mul - multiplies two integers
+*@a: integer to be multiplied
+*@b: integer ro be multiplied
+*Return: return the multiplication of two integers
+*/
+
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
